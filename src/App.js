e096388842eb3c6605ee0cd3f13aedd63929574f
Yuvaraj643 <vyuvarajnani2001@gmail.com>
2023-09-22 19:13:18 +0530

    now

--- src/App.js
+++ src/App.js
@@ -21,7 +21,7 @@ import { NextUIProvider } from "@nextui-org/react";
 import useDarkMode from "use-dark-mode";
 import 'react-dotenv';
 
-export const API_URL = "process.env.API_URL";
+export const API_URL = "https://instagram-83t5.onrender.com";
 // export const API_URL = "http://localhost:5000";
 
 
